86d3116a39ae97d5fea59999de54757cba5259a1
songrise <1138561926@qq.com>
2022-04-03 17:14:54 +0800

    initial commit, added dataset

---
+++ basic_task.py
@@ -0,0 +1,16 @@
+# -*- coding : utf-8 -*-
+# @FileName  : basic_task.py
+# @Author    : Ruixiang JIANG (Songrise)
+# @Time      : Apr 03, 2022
+# @Github    : https://github.com/songrise
+# @Description: script for basic task (task 1-5)
+
+# %% import modules
+import PIL.Image as Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+# import tensorflow as tf
+import cv2
+import sklearn as sk
+#%%
